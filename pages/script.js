7807a6880faf3418136dc40b94b292389ab69ba9
Brunosno <brunosno20@gmail.com>
2024-09-26 17:41:07 -0300

    Upload

---
+++ pages/script.js
@@ -0,0 +1,16 @@
+const key = "790d40eda2e2c854fe34ea672e59db03";
+
+function search(){
+    let city = document.querySelector(".text-city").value;
+    insertInto(city)
+}
+
+async function insertInto(city){
+    let dates = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(answer => answer.json())
+    console.log(dates)
+    document.querySelector(".city-name").innerHTML = "Em " + dates.name;
+    document.querySelector(".temp").innerHTML = Math.floor(dates.main.temp) + "°C";
+    document.querySelector(".img-temp").src = `https://openweathermap.org/img/wn/${dates.weather[0].icon}.png`;
+    document.querySelector(".weather").innerHTML = dates.weather[0].description;
+    document.querySelector(".humidity").innerHTML = "Humidade " + dates.main.humidity + "%";
+}
