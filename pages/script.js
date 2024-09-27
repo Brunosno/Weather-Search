5f76c5de96187b15e24aaefb50f0aaaf346ec2c4
Brunosno <brunosno20@gmail.com>
2024-09-27 15:17:10 -0300

    Screen adjustment

--- pages/script.js
+++ pages/script.js
@@ -2,11 +2,13 @@ const key = "790d40eda2e2c854fe34ea672e59db03";
 
 function search(){
     let city = document.querySelector(".text-city").value;
-    insertInto(city)
+    insertInto(city);
 }
 
 async function insertInto(city){
-    let dates = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(answer => answer.json())
+    let dates = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(answer => answer.json());
+    let infoWeather = document.querySelector(".info-weather");
+    infoWeather.classList.add("active");
     console.log(dates)
     document.querySelector(".city-name").innerHTML = "Em " + dates.name;
     document.querySelector(".temp").innerHTML = Math.floor(dates.main.temp) + "°C";
